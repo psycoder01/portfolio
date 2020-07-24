1dcbdaa9fd691ee094637953df8b6a92d6568f2a
psycoder01 <aakashchy13@gmail.com>
2020-07-24 18:38:58 +0545

    mob responsive support added homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 //Importing Images
 import profile from "../assets/profile.png";
 import facebook from "../assets/facebook.webp";
@@ -7,15 +8,15 @@ import devto from "../assets/dev.webp";
 
 export default () => (
     <React.Fragment>
-        <div className="container p-4">
-            <div className="flex flex-row justify-around">
+        <div className="container p-4 mx-auto">
+            <div className="flex flex-col md:flex-row content-center md:justify-around">
                 <img
-                    className="h-72 w-72 bg-white rounded-full shadow-2xl mx-16 p-4"
+                    className="h-64 w-64 md:h-68 md:w-68 lg:h-72 lg:w-72 bg-white rounded-full shadow-2xl mx-auto md:mx-16 p-4"
                     src={profile}
                     alt="profile-img"
                 />
-                <div className="flex flex-col text-2xl text-blue-600 pt-16">
-                    <div className="display-info space-y-6">
+                <div className="flex flex-col mx-auto md:text-xl lg:text-2xl text-blue-600 pt-8 md:pt-16">
+                    <div className="display-info md:space-y-6">
                         <p>Hello ! My Name is Aakash Chaudhary .</p>
                         <p>I'm a Software Developer .</p>
                         <p>
@@ -23,10 +24,9 @@ export default () => (
                             Production !!
                         </p>
                     </div>
-
                     <div>
                         <button
-                            className="bg-gray-600 mt-16 text-white shadow-2xl rounded-lg w-40 h-16"
+                            className="bg-gray-600 md:mt-8 lg:mt-16 text-white shadow-2xl rounded-lg w-24 h-12 md:w-40 md:h-16"
                             style={{ outline: "none" }}
                             onClick={() => {
                                 window.open(
@@ -39,11 +39,10 @@ export default () => (
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row mt-12 space-x-8 mx-auto w-1/5">
+            <div className="flex flex-row mt-4 sm:mt-12 space-x-8 mx-auto w-64">
                 <a href="https://www.facebook.com/aakash.chaudhary.13">
                     <img src={facebook} alt="" />
                 </a>
-
                 <a href="https://www.github.com/psycoder01">
                     <img src={github} alt="" />
                 </a>
