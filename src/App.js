13d2404302ab09ca4c9d1727e9a0b4ffe0b29333
Psycoder01 <aakashchy13@gmail.com>
2020-09-24 22:56:59 +0545

    :lipstick: added animations

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 //Components
-import Header from "./components/layout/Header";
+import Header from './components/layout/Header'
 
 //Pages
-import Index from "./pages/index";
-import Skills from './pages/skills';
-import Projects from './pages/projects';
-import Contact from './pages/contact';
+import Index from './pages/index'
+import Skills from './pages/skills'
+import Projects from './pages/projects'
+import Contact from './pages/contact'
 
-import "./styles/main.css";
-import "./styles/custom.css";
+import './styles/main.css'
+import './styles/custom.css'
 
 function App() {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <Router>
             <Header />
-            <Route path="/" exact component={Index}/>
-            <Route path="/skills"  component={Skills}/>
-            <Route path="/projects"  component={Projects}/>
-            <Route path="/contact"  component={Contact}/>
+            <Route path="/" exact component={Index} />
+            <Route path="/skills" component={Skills} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
         </Router>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,71 @@
-import React from "react";
+import React from 'react'
 
 //Importing Images
-import profile from "../assets/profile.png";
-import facebook from "../assets/facebook.webp";
-import github from "../assets/github.webp";
-import devto from "../assets/dev.webp";
+import profile from '../assets/profile.jpg'
+import facebook from '../assets/facebook.webp'
+import github from '../assets/github.webp'
+import devto from '../assets/dev.webp'
+
+import dev from '../assets/illustrations/dev.svg'
+
+import Typewriter from 'typewriter-effect'
 
 export default () => (
     <React.Fragment>
         <div className="container p-4 mx-auto">
-            <div className="flex flex-col md:flex-row content-center md:justify-around">
+            <div className="flex flex-col md:flex-row content-center md:justify-around" >
                 <img
                     className="h-64 w-64 md:h-68 md:w-68 lg:h-72 lg:w-72 bg-white rounded-full shadow-2xl mx-auto md:mx-16 p-4"
                     src={profile}
                     alt="profile-img"
+                    data-aos="flip-right"
+                    data-aos-delay="1000"
+                    data-aos-duration="3000"
                 />
-                <div className="flex flex-col mx-auto md:text-xl lg:text-2xl text-blue-600 pt-8 md:pt-16">
+                <div className="flex flex-col mx-auto md:text-xl lg:text-2xl text-teal-900 pt-8 md:pt-16">
                     <div className="display-info md:space-y-6">
-                        <p>Hello ! My Name is Aakash Chaudhary .</p>
-                        <p>I'm a Software Developer .</p>
-                        <p>
-                            I can help you to make your ideas roll into
-                            Production !!
-                        </p>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .pauseFor(2000)
+                                    .typeString('Hello !')
+                                    .pauseFor(1500)
+                                    .typeString('  My Name is ')
+                                    .typeString(' Psycoder01')
+                                    .deleteChars(10)
+                                    .typeString(' Aakash Chaudhary .')
+                                    .pauseFor(5000)
+                                    .start()
+                            }}
+                        />
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .pauseFor(13000)
+                                    .typeString("I'm a Software Developer .")
+                                    .start()
+                            }}
+                        />
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .pauseFor(17000)
+                                    .typeString(
+                                        'I can help you to make your ideas work in real world !!'
+                                    )
+                                    .start()
+                            }}
+                        />
+                        <p></p>
                     </div>
                     <div>
                         <button
                             className="bg-gray-600 md:mt-8 lg:mt-16 text-white shadow-2xl rounded-lg w-24 h-12 md:w-40 md:h-16"
-                            style={{ outline: "none" }}
+                            style={{ outline: 'none' }}
                             onClick={() => {
                                 window.open(
-                                    "mailto:aakashchy13@gmail.com?subject=Summoned"
-                                );
+                                    'mailto:aakashchy13@gmail.com?subject=Summoned'
+                                )
                             }}
                         >
                             Hire ME
@@ -39,6 +73,17 @@ export default () => (
                     </div>
                 </div>
             </div>
+            <img
+                src={dev}
+                className="hidden sm:block w-64 h-64 lg:h-64 lg:w-72"
+                alt="svg"
+                style={{
+                    position: 'absolute',
+                    top:"50vh",
+                    right:0,
+                    zIndex: '-10',
+                }}
+            />
             <div className="flex flex-row mt-4 sm:mt-12 space-x-8 mx-auto w-64">
                 <a href="https://www.facebook.com/aakash.chaudhary.13">
                     <img src={facebook} alt="" />
@@ -52,4 +97,4 @@ export default () => (
             </div>
         </div>
     </React.Fragment>
-);
+)
